ccdefec7d0d46252845223a5af4388082faa8c76
V VASHNAVI <V VASHNAVI@DESKTOP-L191BH0>
2024-06-06 14:47:28 +0530

    helloooooooooooo

---
+++ sample/src/org/sample/PrimitiveDataTypesDemo.java
@@ -0,0 +1,29 @@
+
+
+public class PrimitiveDataTypesDemo {
+	public ststic void main(Strings[] args) {
+		
+		byte byteMax = 127;
+		byte byteMin = -128;
+		
+		System.out.println("Min range of byte is" + byteMin + "Max range of byte is" + byteMax);
+		
+		short shortMax = 32767;
+		short shortMin = -32767;
+		
+		System.out.println("Min range of short is" + shortMin + "Max range of short is" + shortMax);
+		
+		int intMax = 214783647;
+		int intMin = -214783647;
+		
+		System.out.println("Min range of int is" + intMin + "Max range of byte is" + intMax);
+		
+		
+		long longMax = 933225678998786;
+		long longMin = -933225678998786;
+		
+		System.out.println("Min range of long is" + longMin + "Max range of long is" + longMax);
+		
+	}
+
+}
